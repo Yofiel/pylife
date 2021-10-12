c296e58ea33c2e9f43d7ef4fcc1bfd2c97d9ab55
Pedro Gobira <pedrogobira@tutanota.com>
2021-10-12 16:33:00 -0300

    feat: add color picker view and player movement

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from tkinter import messagebox
 
 import pygame
 
-from service import create_players, init_path, sort_players, spin_roulette, start_round
+from service import (
+    check_winner,
+    create_players,
+    init_path,
+    is_over,
+    sort_players,
+    spin_roulette,
+    start_round,
+)
 
 # tamanhos
 WIDTH = 800
@@ -15,6 +23,8 @@ COLOR_BLACK = pygame.Color(0, 0, 0)
 COLOR_WHITE = pygame.Color(255, 255, 255)
 COLOR_GREEN = pygame.Color(1, 91, 32)
 COLOR_RED = pygame.Color(240, 0, 0)
+COLOR_BLUE = pygame.Color(0, 0, 255)
+COLOR_YELLOW = pygame.Color(255, 255, 0)
 
 # inicializando jogo
 pygame.init()
@@ -33,57 +43,57 @@ FONT = pygame.font.Font(None, 32)
 BOARD = pygame.image.load("assets/map.png")
 
 # posicao caminho
-caminho = [
-    (93, 558),
-    (115, 509),
-    (97, 451),
-    (112, 402),
-    (71, 362),
-    (67, 305),
-    (81, 250),
-    (65, 207),
-    (123, 199),
-    (88, 150),
-    (65, 102),
-    (82, 60),
-    (135, 34),
-    (139, 83),
-    (162, 117),
-    (188, 164),
-    (230, 137),
-    (236, 94),
-    (231, 50),
-    (305, 52),
-    (318, 92),
-    (358, 62),
-    (389, 102),
-    (358, 139),
-    (316, 174),
-    (294, 216),
-    (278, 263),
-    (274, 310),
-    (316, 353),
-    (348, 389),
-    (368, 427),
-    (389, 466),
-    (410, 502),
-    (442, 470),
-    (428, 432),
-    (437, 400),
-    (484, 387),
-    (434, 356),
-    (473, 328),
-    (369, 293),
-    (428, 284),
-    (482, 276),
-    (535, 264),
-    (576, 232),
-    (630, 250),
-    (640, 285),
-    (588, 303),
-    (616, 340),
-    (638, 378),
-    (638, 422),
+POSITIONS = [
+    (83, 538),
+    (105, 489),
+    (87, 431),
+    (102, 382),
+    (61, 342),
+    (57, 285),
+    (71, 230),
+    (55, 187),
+    (113, 179),
+    (78, 130),
+    (55, 82),
+    (72, 40),
+    (125, 14),
+    (129, 63),
+    (152, 97),
+    (178, 144),
+    (220, 117),
+    (226, 74),
+    (221, 30),
+    (295, 32),
+    (308, 72),
+    (348, 42),
+    (379, 82),
+    (348, 119),
+    (306, 154),
+    (284, 196),
+    (268, 243),
+    (264, 290),
+    (306, 333),
+    (338, 369),
+    (358, 407),
+    (379, 446),
+    (400, 482),
+    (432, 450),
+    (418, 412),
+    (427, 380),
+    (474, 367),
+    (424, 336),
+    (463, 308),
+    (359, 273),
+    (418, 264),
+    (472, 256),
+    (525, 244),
+    (566, 212),
+    (620, 230),
+    (630, 265),
+    (578, 283),
+    (606, 320),
+    (628, 358),
+    (628, 402),
 ]
 
 
@@ -152,7 +162,7 @@ def name_input_view():
                     if event.key == pygame.K_BACKSPACE:
                         user_text = user_text[:-1]
                     else:
-                        if len(user_text) < 25:
+                        if len(user_text) < 10:
                             user_text += event.unicode
 
         # preenchendo background com cor
@@ -173,14 +183,70 @@ def name_input_view():
         # desenhando retangulo e texto do input na tela
         draw_text_box(WIN, input_text, input_rect, input_rect_current_color, 2)
 
-        # caixa de input acompanha o tamanho da camada de texto
-        input_rect.w = max(140, input_text.get_width() + 10)
-
         # desenhando botao
         draw_text_box(WIN, button_text, button_rect, COLOR_GREEN)
 
         # atualizando tela
         pygame.display.update()
+
+
+def color_input_view(players):
+    colors = []
+
+    blue_rect = create_text_box(200, 230, 40, 40)
+    red_rect = create_text_box(250, 230, 40, 40)
+    green_rect = create_text_box(300, 230, 40, 40)
+    yellow_rect = create_text_box(350, 230, 40, 40)
+
+    current_player = -1
+    while True:
+        if current_player == 3:
+            return colors
+
+        current_player += 1
+
+        # preenchendo background com cor
+        WIN.fill(COLOR_BLACK)
+
+        info_text = create_text(
+            f"{players[current_player]}, escolha uma cor", COLOR_WHITE
+        )
+
+        WIN.blit(info_text, (200, 200))
+
+        pygame.draw.rect(WIN, COLOR_BLUE, blue_rect)
+        pygame.draw.rect(WIN, COLOR_RED, red_rect)
+        pygame.draw.rect(WIN, COLOR_GREEN, green_rect)
+        pygame.draw.rect(WIN, COLOR_YELLOW, yellow_rect)
+
+        # atualizando tela
+        pygame.display.update()
+
+        event_happened = False
+        while not event_happened:
+            event = pygame.event.wait()
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if blue_rect.collidepoint(event.pos):
+                    color = "azul"
+                elif red_rect.collidepoint(event.pos):
+                    color = "vermelho"
+                elif green_rect.collidepoint(event.pos):
+                    color = "verde"
+                elif yellow_rect.collidepoint(event.pos):
+                    color = "amarelo"
+                else:
+                    continue
+
+                if color in colors:
+                    send_message("Essa cor já foi escolhida. Escolha outra!")
+                else:
+                    colors.append(color)
+                    event_happened = True
 
 
 def send_message(message, title="Aviso"):
@@ -191,26 +257,64 @@ def send_message(message, title="Aviso"):
     root.destroy()
 
 
-def board_view(players, path, removed_players):
+def init_players_sprites(players):
+    players_sprites = []
+
+    for player in players:
+        player_sprite = pygame.sprite.Sprite()
+        player_sprite.image = pygame.image.load(f"assets/{player.color}.svg")
+        player_sprite.rect = player_sprite.image.get_rect()
+        players_sprites.append(player_sprite)
+
+    return players_sprites
+
+
+def blit_players(players, players_sprites):
+    for index, sprite in enumerate(players_sprites):
+        sprite.rect.topleft = POSITIONS[players[index].position]
+
+        if pygame.sprite.spritecollideany(sprite, players_sprites):
+            sprite.rect.x += index * 5
+
+        WIN.blit(sprite.image, sprite.rect)
+
+
+def board_view(players, players_sprites, path, removed_players):
     # definicao do botao da roleta
     spin_roulette_text = create_text("SORTEAR NUMERO", COLOR_WHITE)
     spin_roulette_rect = create_text_box(300, 560)
 
     current_player = -1
     while True:
-        if current_player == 3:
+        if current_player == len(players) - 1:
             current_player = -1
 
         current_player += 1
 
-        for event in pygame.event.get():
+        # preenchendo background com imagem
+        WIN.blit(BOARD, (0, 0))
+
+        # desenhando retangulo e texto do input na tela
+        spin_roulette_rect.w = spin_roulette_text.get_width() + 10
+        draw_text_box(WIN, spin_roulette_text, spin_roulette_rect, COLOR_BLACK)
+
+        blit_players(players, players_sprites)
+
+        # atualizando tela
+        pygame.display.update()
+
+        if is_over(players[current_player], removed_players):
+            winner = check_winner(players)
+            send_message("Jogo finalizado!", "Fim da jornada")
+            send_message(f"{winner.name}, você é o vencedor", "Parabéns")
+
+        event_happened = False
+        while not event_happened:
+            event = pygame.event.wait()
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    return
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if spin_roulette_rect.collidepoint(event.pos):
@@ -220,16 +324,12 @@ def board_view(players, path, removed_players):
                         path, players[current_player], removed_players, drawn_number
                     )
                     print(players[current_player])
+                    event_happened = True
 
-        # preenchendo background com imagem
-        WIN.blit(BOARD, (0, 0))
-
-        # desenhando retangulo e texto do input na tela
-        spin_roulette_rect.w = spin_roulette_text.get_width() + 10
-        draw_text_box(WIN, spin_roulette_text, spin_roulette_rect, COLOR_BLACK)
-
-        # atualizando tela
-        pygame.display.update()
+        if players[current_player] in removed_players:
+            players.pop(current_player)
+            players_sprites.pop(current_player)
+            current_player -= 1
 
 
 def main():
@@ -244,12 +344,14 @@ def main():
                 pygame.quit()
 
         names = name_input_view()
-        players = create_players(names)
+        colors = color_input_view(names)
+        players = create_players(names, colors)
         sort_players(players)
+        players_sprites = init_players_sprites(players)
         path = init_path()
         removed_players = []
 
-        board_view(players, path, removed_players)
+        board_view(players, players_sprites, path, removed_players)
 
         pygame.display.update()
 
